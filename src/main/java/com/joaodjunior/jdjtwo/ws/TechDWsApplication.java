161bb454ff1b20b71889cb244134560bfbd79017
14208 - João Luiz Gadelha Dias Junior <14208@DG06E192.adm.bancoamazonia.sa>
2018-07-02 11:51:49 -0300

    Commit inicial
    Signed-off-by: 14208 - João Luiz Gadelha Dias Junior <14208@DG06E192.adm.bancoamazonia.sa>

---
+++ src/main/java/com/joaodjunior/jdjtwo/ws/TechDWsApplication.java
@@ -0,0 +1,12 @@
+package com.joaodjunior.jdjtwo.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechDWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechDWsApplication.class, args);
+	}
+}
